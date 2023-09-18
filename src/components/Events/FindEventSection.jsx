88beefcc73bc828d796a7eda04108ec4ae77f35d
Zukhriddinbek-Code <zuhriddinganiyev2000@gmail.com>
2023-09-18 22:09:54 +0900

    setting enabled prop to send queries and limit them by disabling

--- src/components/Events/FindEventSection.jsx
+++ src/components/Events/FindEventSection.jsx
@@ -1,19 +1,49 @@
 import { useRef, useState } from "react";
 import { useQuery } from "@tanstack/react-query";
 import { fetchEvents } from "../../../util/http";
+import LoadingIndicator from "../UI/LoadingIndicator";
+import ErrorBlock from "../UI/ErrorBlock";
+import EventItem from "./EventItem";
 
 export default function FindEventSection() {
-  const [searchTerm, setSearchTerm] = useState("");
+  const [searchTerm, setSearchTerm] = useState();
   const searchElement = useRef();
 
-  useQuery({
+  const { data, isLoading, isError, error } = useQuery({
     queryKey: ["events", { search: searchTerm }],
-    queryFn: () => fetchEvents(searchTerm),
+    queryFn: ({ signal }) => fetchEvents({ signal, searchTerm }),
+    enabled: searchTerm !== undefined,
   });
 
   function handleSubmit(event) {
     event.preventDefault();
     setSearchTerm(searchElement.current.value);
+  }
+
+  let content = <p>Please enter a search term and to find events.</p>;
+  if (isLoading) {
+    content = <LoadingIndicator />;
+  }
+
+  if (isError) {
+    content = (
+      <ErrorBlock
+        title={"An error occured!"}
+        message={error.info?.message || "Failed to fetch events!"}
+      />
+    );
+  }
+
+  if (data) {
+    content = (
+      <ul className="events-list">
+        {data.map((event) => (
+          <li key={event.id}>
+            <EventItem event={event} />
+          </li>
+        ))}
+      </ul>
+    );
   }
 
   return (
@@ -29,7 +59,7 @@ export default function FindEventSection() {
           <button>Search</button>
         </form>
       </header>
-      <p>Please enter a search term and to find events.</p>
+      {content}
     </section>
   );
 }
